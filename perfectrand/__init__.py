2d8c15483a3b6f066e81aa255d6f310f63cf5cd2
Marek Lisy <marek.lisy@centrum.cz>
2015-05-12 16:44:11 +0200

    initial commit

---
+++ perfectrand/__init__.py
@@ -0,0 +1,2 @@
+def randint(a = 0, b = -1):
+	return 42
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='perfectrand',
+      version='0.1',
+      description='The perfect random number generator',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='marek.lisy@centrum.cz',
+      license='MIT',
+      packages=['perfectrand'],
+      zip_safe=False)
